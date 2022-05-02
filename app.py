f168012a57779c724669a4559e037c1b7edf84cc
Rudransh Srivastava <84043023+RUDRANSH-hub@users.noreply.github.com>
2022-05-03 01:44:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,19 +26,22 @@ elif(add_selectbox=="Student Details"):
     new_df = pd.read_csv("Kamla Nehru Institute of Technology, Sultanpur [29 Apr].csv")
     new_df=new_df.set_index("Student Name")
     # new_df=new_df.style.hide_index()
-    new_df["mine"]=0
+    new_df["Mine1"]="NO"
+    new_df["Mine2"]="NO"
+    new_df["Mine3"]="NO"
+    new_df["Mine4"]="NO"
 
     p=list(new_df["# of Quests Completed"])
     q=list(new_df["# of Skill Badges Completed"])
     for i in range(len(new_df["# of Quests Completed"])):
         if(q[i]==20 and p[i]==40 ):
-            new_df["mine"][i]=4
+            new_df["Mine4"][i]="YES"
         elif( p[i]>=30 and q[i]>=15 ):
-            new_df["mine"][i]=3
+            new_df["Mine3"][i]="YES"
         elif( p[i]>=20 and q[i]>=10 ):
-            new_df["mine"][i]=2
+            new_df["Mine2"][i]="YES"
         elif(p[i]>=10 and q[i]>=5):
-            new_df["mine"][i]=1
+            new_df["Mine1"][i]="YES"
     
     
 
@@ -46,7 +49,7 @@ elif(add_selectbox=="Student Details"):
         
     email=st.text_input("Enter your mail")
     res=get_info(str(email),new_df)
-    res=res.drop(columns=["Student Email","Institution","Enrolment Date & Time","Enrolment Status"],axis=1)
+    res=res.drop(columns=["Student Email","Google Cloud Skills Boost Profile URL","Institution","Enrolment Date & Time","Enrolment Status"],axis=1)
     Button=st.button("Get_data")
     if Button :
         st.table(res)
